025d02f9444cca912f986f3ca28fc554f90e4a43
Dominik Bamberger <b4mboo@users.noreply.github.com>
2017-04-21 14:57:19 -0700

    Create prepare.rb

---
+++ prepare.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+# Variables which may need to be configurable at some point.
+hostname = ARGV[0]
+password = ARGV[1]
+protocol = 'https://'
+port = 8443
+license = '/home/ec2-user/licenses/ghe-license.ghl'
+
+# Variables which are simply created to reduce code duplication.
+curl = 'curl -L -k -X POST'
+api = '/setup/api'
+url = "#{hostname}:#{port}"
+login = "api_key:#{password}"
+path = "#{protocol}#{login}@#{url}#{api}"
+
+# API calls to setup the new GitHub Enterprise instance.
+system "#{curl} '#{protocol}#{url}#{api}/start' -F license=@#{license} -F 'password=#{password}'"
+system "#{curl} '#{path}/configure'"
+check = lambda { `curl -L -k '#{path}/configcheck'` }
+status = ''
+while status.scan(/DONE/).count != 5
+  puts 'Waiting...'
+  sleep 5
+  status = check.call
+  puts status
+end
+# TODO: Iterate over the keys directory.
+['b4mboo'].each do |key|
+  system "#{curl} '#{path}/settings/authorized-keys' -F authorized_key=@/home/ec2-user/keys/#{key}.pub"
+end
+
+# Restore data from backup.
+system "#{curl} '#{path}/maintenance' -d 'maintenance={\"enabled\":true, \"when\":\"now\"}'"
+system "/home/ec2-user/backup-utils-master/bin/ghe-restore -v -f -s 2.7.3 #{hostname}"
+system "#{curl} '#{path}/maintenance' -d 'maintenance={\"enabled\":false, \"when\":\"now\"}'"
